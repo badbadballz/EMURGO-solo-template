bcd9f92ab4807216cb0b0b7a7436645ff6b94af0
badbadballz <roy.chan22@gmail.com>
2023-05-31 22:32:05 +0900

    rewrote for double step

--- src/Types.hs
+++ src/Types.hs
@@ -9,18 +9,31 @@ import Test.QuickCheck
 
 --import qualified Sandbox as S 
 
-data Rotor = Rotor RotorPos StartPos RingSetting RotorWiring deriving (Eq, Show)
+data Rotor = Rotor { rotorPos :: Int
+                   , startPos :: Int
+                   , ringSetting :: Int 
+                   , turns :: Int 
+                   , turnover :: Int 
+                   , rotorWiring :: RotorWiring} deriving (Eq, Show) 
+
+--data Rotor = Rotor RotorPos StartPos RingSetting Turns Turnover RotorWiring deriving (Eq, Show)
+
 type Rotors = [Rotor]
 type Reflector = Rotor
 type Plugboard = Rotor
+
 
 instance Arbitrary Rotor where
     arbitrary = do
                  n <- choose (-1, 10)
                  st <- choose (0, 25)
                  rs <- choose (0, 25)
+                 turnover <- choose (0,25)
                  rw <- shuffle [0..25] -- this needs to change for different rotor inputs/outputs
-                 return $ Rotor n st rs (ringSetting rs rw)
+                 return $ Rotor n st rs st turnover (setRing rs rw)
+
+
+ 
 
 --sToG :: IO String -> Gen String
 --sToG s = s >>= (\s' -> return s')
@@ -33,11 +46,11 @@ instance Arbitrary Rotor where
 -- Also double stepping, which i don't completely understand yet
 
 type Steps = Int
-
-type Turnover = Int
-type RingSetting = Int
-type StartPos = Int
-type RotorPos = Int
+--type Turns = Int
+--type Turnover = Int
+--type RingSetting = Int
+--type StartPos = Int
+--type RotorPos = Int
 type RotorWiring = [Int]
 
 type Letter = Int 
@@ -62,10 +75,11 @@ passRotor' l (stps, r) = let offsetStps = (l + stps) `mod` rotorSize
 -}
 
 -- encrypt is the same as decrypt
+-- encrypt assumes that rotors stack has been created properly (with correct turns)
 encrypt :: Letter -> State MachineState Letter
 encrypt l = state (\s@(stps, outputs, rs) -> 
                     if null rs then (l, s) else
-                    let result = passRotor l stps (head rs)
+                    let result = passRotor l (head rs)
                     in (result, (stps, result:outputs, tail rs)))
 
 pressKey :: Letter -> MachineState -> MachineState
@@ -75,24 +89,81 @@ pressKey l (stps, _ ,rs) = go l (stps+1, [], rs)
                               go l' s@(stps', output, rs') = let (result, s') = runState (encrypt l') s
                                                               in go result s'
 
+ 
+
+
 makeState :: MachineState
 makeState = (0, [], makeRotors)
 
 makeState1 :: MachineState
 makeState1 = (0, [], makeRotors1)
 
-stepsToNthRSteps :: Int -> Int -> Int
-stepsToNthRSteps stps n = (stps `div` rotorSize^n) `mod` rotorSize
+--stepsToNthRSteps :: Int -> Int -> Int
+--stepsToNthRSteps stps n = (stps `div` rotorSize^n) `mod` rotorSize
 
 --stepsTurnoverCheck :: Int
 
-passRotor :: Letter -> Steps -> Rotor -> Letter
+
+passRotor :: Letter -> Rotor -> Letter
+passRotor l r 
+  | rotorPos r == (-1) = rotorWiring r !! l -- plugboards and reflectors
+  | otherwise = let offset = turns r
+                    l' = rotorWiring r !! ((l + offset) `mod` rotorSize)
+                in (l' - offset) `mod` rotorSize
+
+
+{-
+passRotor :: Letter -> Rotor -> Letter
 passRotor l _ (Rotor (-1) _ _ r) = r !! l -- plugboards and reflectors
 passRotor l stps (Rotor n st _ r) = let rStps = stepsToNthRSteps (stps + st) n 
                                         offsetStps = (l + rStps) `mod` rotorSize
                                     in ((r !! offsetStps) - rStps) `mod` rotorSize
+-}
+-- processes the next step, ie assumes the next key has been presses. Stps has already been 
+-- advanced once
+nextRotorsTurns :: Rotors -> Rotors
+nextRotorsTurns rss = go rss False
+                            where
+                            go [] _ = []
+                            go (r:rs) notch
+                                | rotorPos r == 0 = let (r0, notch') = setRotor0 (r, notch) 
+                                                    in r0 : go rs notch'
+                                | rotorPos r == 1 = let (r1, notch') = setRotor1 (r, notch) 
+                                                    in r1 : go rs notch'
+                                | rotorPos r == 2 = let (r2, notch') = setRotor2 (r, notch) 
+                                                    in r2 : go rs notch'
+                                | otherwise = r : go rs False
 
-{-
+-- stps - 1 == turnover r = let offsetStps = (startPos r + stps) `mod` rotorSize
+-- in r{turns = offsetStps} : go stps rs True
+
+
+-- stps - 1 == turnover r &&
+-- notch = let offsetStps = (startPos r + 1) `mod` rotorSize
+-- in r{turns = offsetStps} : go stps rs True
+
+setRotor0 :: (Rotor, Bool) -> (Rotor, Bool)
+setRotor0 (r, n) 
+  | turns r == turnover r = let r' = r{turns = (turns r + 1) `mod` rotorSize}
+                            in  (r', True)
+  | otherwise = let r' = r{turns = (turns r + 1) `mod` rotorSize}
+                in  (r', False)
+
+setRotor1 :: (Rotor, Bool) -> (Rotor, Bool)
+setRotor1 (r, n)
+  | turns r == turnover r = let r' = r{turns = (turns r + 1) `mod` rotorSize}
+                            in  (r', True)
+  | n = let r' = r{turns = (turns r + 1) `mod` rotorSize}
+        in  (r', False)
+  | otherwise = (r, False)
+
+setRotor2 :: (Rotor, Bool) -> (Rotor, Bool)
+setRotor2 (r, n) 
+  | n = let r' = r{turns = (turns r + 1) `mod` rotorSize}
+        in  (r', False)
+  | otherwise = (r, False)
+
+{-                                              
 passRotorLToR :: Letter -> Steps -> Rotor -> Letter
 passRotorLToR l stps (Rotor (-1) _ r') = let rStps = stepsToNthRSteps stps 0
                                              offsetStps = (l + rStps) `mod` rotorSize
@@ -109,6 +180,13 @@ makeRotors = [rotorI, reflectorB, invRotorI]
 
 makeRotors1 :: Rotors
 makeRotors1 = [plugboard, rotorI, rotorII, rotorIII, reflectorB, invRotorIII, invRotorII, invRotorI, plugboard]
+
+makeRotors2 :: Rotors
+makeRotors2 = [plugboard, rotorI, rotorII, rotorIII, reflectorB]
+
+makeRotors3 :: Rotors
+makeRotors3 = [plugboard, rotorIV, rotorV, rotorI, reflectorB]
+
 
 {-
 testRotor :: Rotor -> Bool 
@@ -146,33 +224,56 @@ charToInt c = ord c - upperAlphaOffset
 intToChar :: Int -> Char
 intToChar n = chr $ n + upperAlphaOffset
 
+{-
+data Rotor = Rotor { rotorPos :: Int
+                   , startPos :: Int
+                   , ringSetting :: Int 
+                   , turns :: Int 
+                   , turnover :: Int 
+                   , rotorWiring :: [Int]} deriving (Eq, Show) -}
 
 -- 0 is just a placeholder
+-- rotorPos is the relative position in the enigma machine, not the rotor number
+-- startPos has to be initialised to turns when starting rotor
 rotorI :: Rotor
-rotorI = Rotor 0 3 1 (ringSetting 1 testRotorI) 
+rotorI = Rotor 0 0 0 0 16 (setRing 0 testRotorI) 
+--Rotor 0 3 1 (ringSetting 1 testRotorI) 
 
 invRotorI :: Rotor
-invRotorI = Rotor 0 3 1 (ringSetting 1 invTestRotorI)
+invRotorI = Rotor 0 0 0 0 16 (setRing 0 invTestRotorI)
 
 rotorII :: Rotor
-rotorII = Rotor 1 0 0 testRotorII 
+rotorII = Rotor 1 0 0 0 4 (setRing 0 testRotorII) 
 
 invRotorII :: Rotor
-invRotorII = Rotor 1 0 0 invTestRotorII
+invRotorII = Rotor 1 0 0 0 4 (setRing 0 invTestRotorII)
 
 rotorIII :: Rotor
-rotorIII = Rotor 2 0 0 testRotorIII 
+rotorIII = Rotor 2 0 0 0 21 (setRing 0 testRotorIII) 
 
 invRotorIII :: Rotor
-invRotorIII = Rotor 2 0 0 invTestRotorIII
+invRotorIII = Rotor 2 0 0 0 21 (setRing 0 invTestRotorIII)
+
+rotorIV :: Rotor
+rotorIV = Rotor 0 25 0 25 9 (setRing 0 testRotorIV)
+
+invRotorIV :: Rotor
+invRotorIV = Rotor 0 25 0 25 9 (setRing 0 testInvRotorIV)
+
+rotorV :: Rotor
+rotorV = Rotor 1 8 0 8 25 (setRing 0 testRotorV)
+
+invRotorV :: Rotor
+invRotorV = Rotor 1 8 0 8 25 (setRing 0 testInvRotorV)
+
 
 testRotorI :: RotorWiring --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 testRotorI = map charToInt  "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 
 
-ringSetting :: Int -> RotorWiring -> RotorWiring
-ringSetting n rw = let c = cycleList (n `mod` rotorSize) rw
-                   in map (\i -> (i + n) `mod` rotorSize) c
+setRing :: Int -> RotorWiring -> RotorWiring
+setRing n rw = let c = cycleList (n `mod` rotorSize) rw
+               in map (\i -> (i + n) `mod` rotorSize) c
                    
 
 cycleList :: Int -> RotorWiring -> RotorWiring
@@ -196,6 +297,18 @@ testRotorIII = map charToInt  "BDFHJLCPRTXVZNYEIWGAKMUSQO"
 invTestRotorIII :: RotorWiring
 invTestRotorIII = inverseRotorWiring testRotorIII
 
+testRotorIV :: RotorWiring --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+testRotorIV = map charToInt  "ESOVPZJAYQUIRHXLNFTGKDCMWB"
+
+testInvRotorIV :: RotorWiring
+testInvRotorIV = inverseRotorWiring testRotorIV 
+
+testRotorV :: RotorWiring --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+testRotorV = map charToInt  "VZBRGITYUPSDNHLXAWMJQOFECK"
+
+testInvRotorV :: RotorWiring
+testInvRotorV = inverseRotorWiring testRotorV
+
 testReflectorB :: RotorWiring --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 testReflectorB = map charToInt  "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 
@@ -209,7 +322,7 @@ testPlugboard :: RotorWiring --"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 testPlugboard = map charToInt  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 reflectorB :: Reflector
-reflectorB = Rotor (-1) 0 0 testReflectorB 
+reflectorB = Rotor (-1) 0 0 0 0 testReflectorB 
 
 plugboard :: Plugboard
-plugboard = Rotor (-1) 0 0 testPlugboard
+plugboard = Rotor (-1) 0 0 0 0 testPlugboard
