ae91c918fedc51bd61d2dbc2e65a3ac95cf134b9
badbadballz <roy.chan22@gmail.com>
2023-06-12 01:08:38 +0800

    added title

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,14 @@ import Lib
 import Types
 import System.IO (hSetBuffering, stdout, stdin, BufferMode(NoBuffering))
 import Control.Monad.State
+import System.Console.ANSI (setTitle, clearScreen)
 
 main :: IO ()
 main = do
+        clearScreen
         hSetBuffering stdout NoBuffering
         hSetBuffering stdin  NoBuffering
+        setTitle "haskell Enigma M3 simulator"
         putStrLn "Check README for more in-depth information"
         ms <- setMachine' 
         runStateT (printMachine' []) ms
